05fd28738733c67cb971ed3d6fca3dd4ecf6603e
dang.nguyen <dang.nguyen@billbros.vn>
2023-11-06 15:47:39 +0700

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,17 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
+if [ -z "$IMAGE" ]
+then
+	IMAGE='registry.billbros.vn/cooky_python_api:python3_mssql'
+fi
+
+if [ -z "$NUM_WORKERS" ]
+then
+	NUM_WORKERS='multiprocessing.cpu_count() * 2 \+ 1'
+fi
+
 SERVICE_NAME=${PROJECT_NAME}_${HOST_PORT}
-echo $SERVICE_NAME
 
 docker compose up -d
 
